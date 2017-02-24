36d85314c9b92feed8359df9a4ce9de2173882e0
Peter Yoon <peyo.hd@gmail.com>
2017-02-24 10:44:58 +0900

    initial commit

---
+++ src/com/peyo/videoview/MainActivity.java
@@ -0,0 +1,33 @@
+package com.peyo.videoview;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+	private VideoView mVideo1;
+	private VideoView mVideo2;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		mVideo1 = (VideoView) findViewById(R.id.video1);
+		mVideo1.setVideoURI(Uri.parse("http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4"));
+		mVideo1.setMediaController(new MediaController(this));
+
+		mVideo2 = (VideoView) findViewById(R.id.video2);
+		mVideo2.setVideoURI(Uri.parse("http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202012_%20Year%20In%20Review.mp4"));
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+		mVideo1.start();
+		mVideo2.start();
+	}
+}
