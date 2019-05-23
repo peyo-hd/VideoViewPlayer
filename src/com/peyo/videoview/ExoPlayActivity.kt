dd990a575ff0e3527fcd692e6ea462720d89d3a0
Peter Yoon <peyo.hd@gmail.com>
2019-05-23 22:47:06 +0900

    update studio & exo version

--- src/com/peyo/videoview/ExoPlayActivity.kt
+++ src/com/peyo/videoview/ExoPlayActivity.kt
@@ -5,8 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import kotlinx.android.synthetic.main.exo.*
 
@@ -16,16 +15,16 @@ class ExoPlayActivity : Activity() {
         setContentView(R.layout.exo)
     }
 
-    private val URL = "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4"
+    private val URL = "https://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review.mp4"
 
     override fun onResume() {
         super.onResume()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        val player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+        val player = ExoPlayerFactory.newSimpleInstance(this)
         exoPlayerView.player = player
 
-        player.prepare(ExtractorMediaSource.Factory(
+        player.prepare(ProgressiveMediaSource.Factory(
                 DefaultDataSourceFactory(this, "ExoPlayActivity"))
                 .createMediaSource(Uri.parse(URL)))
         player.playWhenReady = true
